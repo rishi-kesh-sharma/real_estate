21aec491bfe271b322a7870aad96f15aa898a4f1
Rishikesh Sharma <wrongdirection72@gmail.com>
2023-11-01 08:20:13 +0545

    working on favorites

--- real_estate_backend_node/src/modules/favourites/controller.js
+++ real_estate_backend_node/src/modules/favourites/controller.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
 const express = require("express");
-const { handleValidation } = require("../../common/middlewares");
+const {
+  handleValidation,
+  authenticateRequest,
+  authorizeRequest,
+} = require("../../common/middlewares");
 const { getQuery } = require("./service");
 const {
   saveHandler,
   deleteHandler,
   searchHandler: baseSearchHandler,
+  countHandler: baseCountHandler,
 } = require("../../core/controller");
 const { validate } = require("./request");
 
@@ -16,13 +21,14 @@ const searchHandler = async (req, res, next) => {
   return baseSearchHandler(req, res, next);
 };
 
-// const countHandler = async (req, res, next) => {
-//   req.searchQuery = getQuery(req.body);
-//   return baseCountHandler(req, res, next);
-// };
+const countHandler = async (req, res, next) => {
+  req.searchQuery = getQuery(req.body);
+  return baseCountHandler(req, res, next);
+};
 
 router.post("/create", handleValidation(validate), saveHandler);
 router.post("/search", searchHandler);
-router.delete("/delete", deleteHandler);
+router.post("/count", authenticateRequest, authorizeRequest, countHandler);
+router.delete("/delete", handleValidation(validate), deleteHandler);
 
 module.exports = router;
